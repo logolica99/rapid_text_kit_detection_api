33d3701e23ca96d1110eef741e5c1680898b9062
Jubaer Jami <jamijubaer@gmail.com>
2024-05-06 19:01:13 +0600

    some updates

--- app.py
+++ app.py
@@ -11,40 +11,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-model_url = "https://www.dropbox.com/scl/fi/ehfy920bd4367pprwe9p6/trained_model.h5?rlkey=wkz3qlc82bym1m0e3jxxfivbs&st=jhgqhe7i&dl=0"
-
-# Create the directory if it doesn't exist
-# model_dir = os.path.dirname("trained_model.h5")
-# model_dir ="trained_model.h5"
-
-
-# try:
-#   # Download the model
-#   response = requests.get(model_url, stream=True)
-#   response.raise_for_status()  # Raise an exception for non-200 status codes
-
-#   # Write the model file
-#   with open("trained_model.h5", 'wb') as f:
-#     for chunk in response.iter_content(1024):
-#       f.write(chunk)
-
-#   # Load the model only if download is successful
-
-#   print("Model loaded successfully!")
-
-# except requests.exceptions.RequestException as e:
-#   print(f"Error downloading model: {e}")
-#   model = None  # Set model to None to indicate failure
-
-# try:
-
-
-#   os.system("wget -O trained_model.h5  https://www.dropbox.com/scl/fi/ehfy920bd4367pprwe9p6/trained_model.h5?rlkey=wkz3qlc82bym1m0e3jxxfivbs&st=jhgqhe7i&dl=0")
-  
-# except requests.exceptions.RequestException as e:
-#   print(f"Error downloading model: {e}")
-#   model = None  # Set model to None to indicate failure
-model = tf.keras.models.load_model('trained_model.h5')
+model = tf.keras.models.load_model('trained_model_robust.h5')
 
 
  
@@ -53,7 +20,7 @@ class_names=['NEGATIVE', 'POSITIVE']
 
 def predict_image(image_path):
   print(image_path)
-  img = tf.keras.preprocessing.image.load_img(image_path, target_size=(256, 256), color_mode="grayscale")
+  img = tf.keras.preprocessing.image.load_img(image_path, target_size=(128, 128), color_mode="grayscale")
   img_array = tf.keras.preprocessing.image.img_to_array(img)
 
   img_tensor = tf.convert_to_tensor(img_array)
@@ -103,9 +70,8 @@ def predict():
 
   predicted_class,confidence = predict_image(img_file_name)
 
-  predictions=[0,23,40]
+  os.remove(img_file_name)
 
 
   return jsonify({'predicted_class':predicted_class,"confidence":confidence})
 
-# Start the Flask server in a new thread
